0936906beabd50933bbc367bff61a82c1c336cc1
kenny <kennyjanroxas73@gmail.com>
2024-12-17 22:13:41 +0800

    added export embeddedform

--- src/components/EmbeddedForm.js
+++ src/components/EmbeddedForm.js
@@ -1,8 +1,10 @@
 import { init, Form } from '@feathery/react';
 
-function App() {
+function EmbeddedForm() {
   // Initialize Feathery
   init('40a3e13c-87c4-4e38-bce0-630f97c35057');
   // Show the Feathery form
   return <Form formId='<7lSXvb>' />
 }
+
+export default EmbeddedForm;
